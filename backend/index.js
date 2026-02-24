dc2749a100a09ea1cab48166864ac8c5c6930ecd
Anzar Jamal <anzar543210@gmail.com>
2026-02-24 20:12:54 +0530

    add note route created

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,15 @@ require("dotenv").config();
 const config = require("./config.json");
 const mongoose = require("mongoose");
 
-mongoose.connect(config.connectionString);
+mongoose.connect(config.connectionString)
+.then(()=>console.log("MongoDb connected"))
+.catch((err)=>console.error("MongoDB error",err));
+
+
 const jwt = require("jsonwebtoken");
 const {authenticateToken} = require("./utilities");
 const User = require("./models/user.model")
+const Note = require("./models/note.model");
 
 
 
@@ -62,7 +67,7 @@ app.post('/create-account',async (req,res)=>{
 
     await user.save();
 
-    const accessToken = jwt.sign({user},process.env.JWT_SECRET,{expiresIn:"36000m"});
+    const accessToken = jwt.sign({_id: user._id, email: user.email},process.env.JWT_SECRET,{expiresIn:"36000m"});
 
     return res.json({
         error:false,
@@ -88,7 +93,7 @@ app.post('/login',async (req,res)=>{
     
     if(userInfo.email == email && userInfo.password==password){
         const user = {user:userInfo};
-        const accessToken = jwt.sign({user},process.env.JWT_SECRET,{expiresIn:"36000m"});
+        const accessToken = jwt.sign({ _id: userInfo._id, email: userInfo.email},process.env.JWT_SECRET,{expiresIn:"36000m"});
 
         return res.json({
             error:false,
@@ -105,6 +110,87 @@ app.post('/login',async (req,res)=>{
         })
     }
 })
+
+
+
+app.post('/add-note',authenticateToken,async (req,res)=>{
+
+    const {title, content, tags} = req.body;
+    const user = req.user;
+
+    if(!title) return res.status(400).json({error:true, message:"title is required"});
+
+    if(!content) return res.status(400).json({error:true, message:"content is required"});
+
+    try{
+        const note = new Note({
+            title,
+            content,
+            tags: tags || [],
+            userId: user._id,
+        });
+
+        await note.save();
+
+        return res.json({
+            error:false,
+            note,
+            message:"Note Added Successfully",
+        });
+
+    }
+
+    catch(error){
+        console.log("error note",error)
+        return res.status(500).json({error:true, message:error.message});
+    }
+})
+
+
+
+app.post('/edit-note/:noteId',authenticateToken,async (req,res)=>{
+
+    const noteId  = req.params.noteId;
+    const {title, content, tags, isPinned} = req.body;
+    const user = req.user;
+
+    if(!title && !conten && !tags){
+        return res.status(400).json({error:true, message:"no changes provided"})
+    }
+
+    try{
+        const note = await Note.findOne({_id:noteId, userId:user._id})
+
+        if(!note) return res.status(400).json({error:true, message:"note not found"});
+
+        if(title) note.title = title;
+        if(content) note.content = content;
+        if(tags) note.tags = tags;
+        if(isPinned) note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error:false,
+            note,
+            message:"Note updated successfully"
+        })
+
+    }
+
+
+    catch(error){
+        console.log(error)
+        return res.status(500)
+        .json({error:true,
+            message:"internal server error"
+        })
+    }
+
+})
+
+
+
 
 
 
